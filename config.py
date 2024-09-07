3721c0feaa8238c88aee61e88edfaf4ed7543588
Shadowxking <lekhraajbansiwal@gmail.com>
2024-09-07 17:35:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ksdofficial8/Kitty-MUSIC",
+    "https://github/Shadowxking/Phonk-music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
